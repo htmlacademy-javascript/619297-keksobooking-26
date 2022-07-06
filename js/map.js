fac7c977f988f60934e579290824b0f712e679a4
Михаил Кононенко <89622842868@mail.ru>
2022-07-06 21:35:23 +1000

    неполадки с картой

---
+++ js/map.js
@@ -0,0 +1,13 @@
+
+const mapCanvas = document.querySelector('.map__canvas');
+const map = L.map('map-canvas').setView({
+  lat: 35.68622,
+  lng: 139.77074,
+}, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
