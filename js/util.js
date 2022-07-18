851a1dc987375dea6c23e4a3f02e22046b30df52
Михаил Кононенко <89622842868@mail.ru>
2022-07-18 21:12:17 +0900

    Сделал 12 модуль часть 1

--- js/util.js
+++ js/util.js
@@ -51,4 +51,12 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {getRandomArrayElement, getRandomArrayElements, getRandomPositiveFloat, getRandomPositiveInteger, makeGenerator, isEscapeKey, showAlert};
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomArrayElement, getRandomArrayElements, getRandomPositiveFloat, getRandomPositiveInteger, makeGenerator, isEscapeKey, showAlert, debounce};
